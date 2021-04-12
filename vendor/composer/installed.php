626076ffe2376de9194e1d2e9b6e1ae42f85043f
Pascal Lehnert <lehnertpascal@gmail.com>
2021-04-12 11:12:51 +0200

    Added vendor folder to repo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'delennerd/delennerd-blocks',
+  ),
+  'versions' => 
+  array (
+    'delennerd/delennerd-blocks' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'yahnis-elsts/plugin-update-checker' => 
+    array (
+      'pretty_version' => 'v4.11',
+      'version' => '4.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3155f2d3f1ca5e7ed3f25b256f020e370515af43',
+    ),
+  ),
+);
