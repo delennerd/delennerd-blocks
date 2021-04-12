b68ce9c88aa062e01068e37046cfc7367828d366
Pascal Lehnert <lehnertpascal@gmail.com>
2021-04-12 11:02:59 +0200

    Variable bug

--- inc/update-checker.php
+++ inc/update-checker.php
@@ -10,10 +10,10 @@ defined( 'ABSPATH' ) || exit;
 
 // require_once DLM_BLOCKS_PATH . '/plugin-update-checker/plugin-update-checker.php';
 
-$dlmUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+$delennedBlocksUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://github.com/delennerd/delennerd-blocks',
 	DLM_BLOCKS_PATH . '/delennerd-blocks.php',
 	'delennerd-blocks'
 );
 
-$myUpdateChecker->setBranch('master');
+$delennedBlocksUpdateChecker->setBranch('master');
